449b63d35d86b0a40f9d6e12050fc1f9d49dd786
joaovsa7 <jvsadesign@gmail.com>
2022-02-09 10:33:49 -0300

    feat: add google analytics to track active users

--- src/App.svelte
+++ src/App.svelte
@@ -5,13 +5,25 @@
 
 	import Board from './components/Board/Board.svelte'
 	import Header from './components/Header/Header.svelte'
+
 </script>
+<svelte:head>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-TX55FRGK82');
+  </script>
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-TX55FRGK82"></script>
+</svelte:head>
 
 <MetaTags
 	title="Adivinhe a Palavra"
 	description="Jogue agora o jogo de adivinhar a palavra"
 	keyword='jogo, term.ooo, wordle, palavras, adivinhar, jogo de adivinhar as palavras'
 />
+
 
 <main>
 	<div class="container">
